0feff775098a2e621b098638d33a80fb31011166
bgsub <bryanngatshou@gmail.com>
2023-08-20 19:36:39 -0400

    Created a Model for a player

---
+++ Tiktaktoe/Tiktaktoe/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tiktaktoe.Data.EnumsData;
+
+namespace Tiktaktoe.Models
+{
+    public class Player
+    {
+        [Key]
+        public string Name { get; set; }
+        public string OpponantName { get; set; }
+        public bool IsOpponentVirtual { get; set; }
+        public GameMode GameMode { get; set; }
+        public int GameDuration { get; set; }
+        public int Wins { get; set; }
+        public int Loses { get; set; }
+        public int Draws { get; set; }
+        public int Score { get; set; }
+
+
+    }
+}
