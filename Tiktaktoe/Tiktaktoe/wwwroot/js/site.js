9855ef512ffca1e42b0528ed5941469454e6f871
bgsub <bryanngatshou@gmail.com>
2023-08-27 20:08:28 -0400

    feat(Game-background): added the title and the game symbols in the back-ground

--- Tiktaktoe/Tiktaktoe/wwwroot/js/site.js
+++ Tiktaktoe/Tiktaktoe/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var fullWidth = window.innerWidth;
+var fullHeight = window.innerHeight;
+
+
+function getRandomPositions() {
+    let randomX = document.getElementById("random_x");
+    let randomO = document.getElementById("random_o");
+    var leftX = Math.round(Math.random() * fullWidth) + 'px';
+    var leftO = Math.round(Math.random() * fullWidth) + 'px';
+    var topX = Math.round(Math.random() * fullHeight) + 'px';
+    var topO = Math.round(Math.random() * fullHeight) + 'px';
+    randomX.style.right = leftX;
+    randomX.style.top = topX;
+    randomO.style.right = leftO;
+    randomO.style.top = topO;
+};
