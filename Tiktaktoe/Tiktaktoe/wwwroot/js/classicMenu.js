186569dfbace835203ad3ffe63b46bcaa2d7b3a8
bgsub <bryanngatshou@gmail.com>
2023-09-13 00:38:13 -0400

    feat(classic-menu): added the inputs and buttons for a classic game

---
+++ Tiktaktoe/Tiktaktoe/wwwroot/js/classicMenu.js
@@ -0,0 +1,7 @@
+﻿function continueTo(nextView) {
+    let classicMenu = document.getElementById("classic-menu");
+    let view = document.getElementById(nextView);
+    classicMenu.style.display = "none";
+    view.style.display = "flex";
+    view.style.flexDirection = "column";
+}
--- Tiktaktoe/Tiktaktoe/wwwroot/js/mainMenu.js
+++ Tiktaktoe/Tiktaktoe/wwwroot/js/mainMenu.js
@@ -5,3 +5,10 @@
     menu.style.display = "flex";
     menu.style.flexDirection = "column";
 }
+function backToMainMenu(menuToClose) {
+    let mainMenu = document.getElementById("main-menu");
+    let menu = document.getElementById(menuToClose);
+    menu.style.display = "none";
+    mainMenu.style.display = "flex";
+    mainMenu.style.flexDirection = "column";
+}
